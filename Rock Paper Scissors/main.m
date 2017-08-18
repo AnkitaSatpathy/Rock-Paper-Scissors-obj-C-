130852cd967c36fa9aa64986a3c112ecb95c0fa7
AnkitaSatpathy <ankitasatpathy94@gmail.com>
2017-08-18 19:19:20 +0530

    Initial Commit

---
+++ Rock Paper Scissors/main.m
@@ -0,0 +1,17 @@
+//
+//  main.m
+//  Rock Paper Scissors
+//
+//  Created by Ankita Satpathy on 18/08/17.
+//  Copyright © 2017 Ankita Satpathy. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+int main(int argc, const char * argv[]) {
+    @autoreleasepool {
+        // insert code here...
+        NSLog(@"Hello, World!");
+    }
+    return 0;
+}
